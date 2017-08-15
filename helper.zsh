5bc314ed076ce530c9c45132fd70ba271971f2b4
Marc Wieland <marcwieland95@gmail.com>
2017-08-15 21:47:54 +0200

    Add `enrol.base` command (fix #11)

--- helper.zsh
+++ helper.zsh
@@ -7,3 +7,8 @@ enrol.helper.finder() { eval open . }
 enrol.reload() {
 	source ~/.zshrc
 }
+
+# Return to base path - set in global config (ER_PROJECT_DIR)
+enrol.base() {
+	eval cd $ER_PROJECT_DIR
+}
